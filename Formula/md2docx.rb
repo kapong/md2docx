ebbec68317fbfd9d90795f79eca61dcd1f231f5c
GitHub Action <action@github.com>
2026-01-30 13:20:16 +0000

    chore: update Homebrew formula to v0.1.1

--- Formula/md2docx.rb
+++ Formula/md2docx.rb
@@ -1,19 +1,19 @@
 class Md2docx < Formula
   desc "Markdown to professional DOCX converter with native Thai support"
   homepage "https://github.com/kapong/md2docx"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-arm64"
-      sha256 "8d6492591f7d2d8195bcfcca724f4730a969913d8211fbb8a31f6deddd9e8f31"
+      sha256 "f7ef91fc2ea8dd94f6006fd3afb0dce141d55ba6f4c8074206ab44985855eda8"
     else
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-x86_64"
-      sha256 "d4a1681d7941b48120bbb867308ec81d08631f4dd3e3c10f0be16f075ed67b71"
+      sha256 "b1386ecdecec331688c734b17dbeee1746d37b31d29cdb75a4769859f904252b"
     end
   elsif OS.linux?
     url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-linux-x86_64"
-    sha256 "c2d13546830c9691d8e7944877460dd26bea7b681490f5a7fc4d4f989cdbf087"
+    sha256 "6d505bf6c1cb309291f9a2526e1e8aab265eb88a24f0d49f0373dff4363ff397"
   end
 
   def install

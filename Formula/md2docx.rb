ff52b89fd91f44b83b1427c514e0b0cbac35d4b5
GitHub Action <action@github.com>
2026-02-13 06:13:20 +0000

    chore: update package manifests to v0.1.10

--- Formula/md2docx.rb
+++ Formula/md2docx.rb
@@ -1,19 +1,19 @@
 class Md2docx < Formula
   desc "Markdown to professional DOCX converter with native Thai support"
   homepage "https://github.com/kapong/md2docx"
-  version "0.1.9"
+  version "0.1.10"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-arm64"
-      sha256 "1459d0d146cd7ab53281355b6640b67c6dc2feadacb5f275b0c1d1ad65aabf13"
+      sha256 "9da6931c0b7e6661df9b9b25f45417b9cac88d7c904e004f1c531446b9f3bc53"
     else
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-x86_64"
-      sha256 "598da80404b0209f25b5ce5251626b273f04bc91c017fd2d0c697a856b397dd1"
+      sha256 "1cfdd4a925261c9e7561728a8611dcdf0513d3cf3bc717b5e553287f8b0244a8"
     end
   elsif OS.linux?
     url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-linux-x86_64"
-    sha256 "deadaf58af9c01095006a96e34af2b666fadeac418ec12a9d498a759c65a29c6"
+    sha256 "e9d8a7a150f9336bc96144106eda608952324ca86a7ee21f6d7750f321e21d35"
   end
 
   def install

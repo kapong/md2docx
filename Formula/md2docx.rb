8ba60cb8d6d9df9350479906832138b7461e792d
GitHub Action <action@github.com>
2026-02-20 14:31:16 +0000

    chore: update package manifests to v0.2.0

--- Formula/md2docx.rb
+++ Formula/md2docx.rb
@@ -6,14 +6,14 @@ class Md2docx < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-arm64"
-      sha256 "9da6931c0b7e6661df9b9b25f45417b9cac88d7c904e004f1c531446b9f3bc53"
+      sha256 "a66da5b9686032e1bb57afed87ea05a561fe37598b47a96354b52e5120c67f24"
     else
       url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-darwin-x86_64"
-      sha256 "1cfdd4a925261c9e7561728a8611dcdf0513d3cf3bc717b5e553287f8b0244a8"
+      sha256 "1a40b690d68588bcd8a8bafffaea10247039f52e198c52888940a1df020aa715"
     end
   elsif OS.linux?
     url "https://github.com/kapong/md2docx/releases/download/v#{version}/md2docx-linux-x86_64"
-    sha256 "e9d8a7a150f9336bc96144106eda608952324ca86a7ee21f6d7750f321e21d35"
+    sha256 "bc49edec1ca943aa4bb681a866039e429413ffbf77bba514254a0a69750452f1"
   end
 
   def install
